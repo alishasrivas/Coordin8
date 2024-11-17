8ded365f1cfda9c3bd74f3b1f9f6f527f4c2345a
Tan Le <letan101104@gmail.com>
2024-11-16 22:07:04 -0500

    Finish SetUpAccountComponent

--- frontend/source/components/SetUpAccountComponent/SetUpAccountComponent.js
+++ frontend/source/components/SetUpAccountComponent/SetUpAccountComponent.js
@@ -17,26 +17,29 @@ export class LogOutComponent extends BaseComponent {
     #getTemplate() {
         return `
             <h2>Coordin8</h2>
-            <form class="log-out">
-                <h3>We are glad to have you!</h3>
+            <form class="set-up">
+                <h3>Please finish the information to set up your account</h3>
                 <div class="input-block">
                     <div class="inner_input">
-                        <label for="email">Email:</label>
-                        <input type="text" id="email">
+                        <label for="username">Username:</label>
+                        <input type="text" id="username">
                     </div>
                     <div class="inner_input">
-                        <label for="password">Password:</label>
-                        <input type="password" id="password">
+                        <label for="primary_time_zone">Primary Time Zone:</label>
+                        <input type="text" id="primary_time_zone">
                     </div>
                     <div class="inner_input">
-                        <label for="re_password">Retype Password:</label>
-                        <input type="password" id="re_password">
+                        <label for="noti_pref">Email Notification:</label>
+                        <label class="switch">
+                            <input type="checkbox" id="noti_pref">
+                            <span class="slider round"></span>
+                        </label>
                     </div>
 
                 </div>
             </form>
-            <button type="submit" class="submit-button">Sign Up</button>
-        `
+            <button type="submit" class="submit-button">Finish</button>       
+         `
     }
     #attachEventListeners() {
         //Add logic for event litseners right here
