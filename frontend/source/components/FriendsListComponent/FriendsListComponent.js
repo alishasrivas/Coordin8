ca8899a6fd1a7e45ec00e18d69962643dc86dc14
joshua041904 <joshuaswaida@gmail.com>
2024-11-17 21:05:53 -0500

    FriendsListComponent: added an import statement for EventHub and Events.

--- frontend/source/components/FriendsListComponent/FriendsListComponent.js
+++ frontend/source/components/FriendsListComponent/FriendsListComponent.js
@@ -1,23 +1,24 @@
-import { BaseComponent } from "../BaseComponent/BaseComponent.js"
-
+import { EventHub } from "../../eventhub/EventHub.js";
+import { Events } from "../../eventhub/Events.js";
+import { BaseComponent } from "../BaseComponent/BaseComponent.js";
 export class FriendsListComponent extends BaseComponent {
-    #container = null;
-    #hub = null;
+  #container = null;
+  #hub = null;
 
-    constructor(){
-        super();
-        this.loadCSS('FriendsListComponent');
-        this.#hub = EventHub.getInstance();
-    }
+  constructor() {
+    super();
+    this.loadCSS("FriendsListComponent");
+    this.#hub = EventHub.getInstance();
+  }
 
-    #buildContainer() {
-        this.#container = document.createElement('div');
-        this.#container.classList.add("friends-list-container");
-        this.#container.innerHTML = this.#getTemplate();
-    }
+  #buildContainer() {
+    this.#container = document.createElement("div");
+    this.#container.classList.add("friends-list-container");
+    this.#container.innerHTML = this.#getTemplate();
+  }
 
-    #getTemplate() {
-        return `
+  #getTemplate() {
+    return `
         <h2>Friends List</h2>
         <div class = "friends-list-search">
             <input type="text" id="friendsSearchInput" placeholder="Search for your friends here...">
@@ -27,30 +28,32 @@ export class FriendsListComponent extends BaseComponent {
         <h3>Your Friends</h3>
         <div class = "friends-display" id = "friendsDisplay"></div>
         `;
-    }
-    #search(query){
-        //add my backend search logic here
-        console.log(`Searching for friends with query: ${query}`)
-        const resultsContainer = this.#container.querySelector("#friends-list-search-results");
-        resultsContainer.innerHTML = `<p>Search results for "${query}" will show here.</p>`;
-    }
+  }
+  #search(query) {
+    //add my backend search logic here
+    console.log(`Searching for friends with query: ${query}`);
+    const resultsContainer = this.#container.querySelector(
+      "#friends-list-search-results"
+    );
+    resultsContainer.innerHTML = `<p>Search results for "${query}" will show here.</p>`;
+  }
 
-    #attachEventListeners() {
-        // Attaching event listeners for searching for friends
-        //SEARCH
-        const searchBar = this.#container.querySelector("#friendsSearchBar");
-        searchBar.addEventListener("click", () => {
-            const query = this.#container.querySelector("#friendsSearchInput").value;
-            this.#search(query);
-        });
-    }
+  #attachEventListeners() {
+    // Attaching event listeners for searching for friends
+    //SEARCH
+    const searchBar = this.#container.querySelector("#friendsSearchBar");
+    searchBar.addEventListener("click", () => {
+      const query = this.#container.querySelector("#friendsSearchInput").value;
+      this.#search(query);
+    });
+  }
 
-    render() {
-        if (this.#container) {
-            return this.#container;
-        }
-        this.#buildContainer();
-        this.#attachEventListeners();
-        return this.#container;
-    } //same as everyone else's render
+  render() {
+    if (this.#container) {
+      return this.#container;
+    }
+    this.#buildContainer();
+    this.#attachEventListeners();
+    return this.#container;
+  } //same as everyone else's render
 }
