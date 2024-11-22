93363127616a39de2268d0bbcac354d85938bbc3
Tan Le <letan101104@gmail.com>
2024-11-21 23:10:58 -0500

    Set up routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,2 +1,16 @@
 //set up all routes here
+//TODO: add routes here
 
+import express from "express";
+import { register, login, logout } from "../controller/controller";
+import { isAuthenticated } from "../middleware/middleware.js";
+
+const router = express.Router();
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout); //A get method is sufficient for logout
+
+
+//TODO: add protected routes here
+//router.get("/api/endpoint", isAutheticated, <function that called when HTTP request is called to endpoint>)
+// router.get("/api", isAuthenticated)
