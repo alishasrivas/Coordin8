f6f7f209a1c77c5ef3a56b3eb6c834f1e0e43e55
Tan Le <letan101104@gmail.com>
2024-12-02 11:33:22 -0500

    authentication issue#49: added error handling for authentication functions

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 //!define main logic and functions for backend here
-//TODO: add try catch blocks for each function
 //load environemtn variables
 dotenv.config({ path: '../.env' });
 const existsUser = async (email) => {
@@ -20,35 +19,49 @@ export const test = async (req, res) => {
 // Registration route.
 // This route creates a new user in the database.
 export const register = async (req, res) => {
-    const { email, password, username, primary_time_zone } = req.body;
-    // Check if the username is already taken
-    if (await existsUser(email))
-        return res.status(400).json(factoryResponse(400, "Your email is linked to an existing account"));
-    const hash = await bcrypt.hash(password, 10);
-    await UserInstance.create({ email, password: hash, username, primary_time_zone });
-    res.json(factoryResponse(200, "Registration successful"));
-    console.log("User registered successfully");
+    try {
+        const { email, password, username, primary_time_zone } = req.body;
+        // Check if the username is already taken
+        if (await existsUser(email))
+            return res.status(400).json(factoryResponse(400, "Your email is linked to an existing account"));
+        const hash = await bcrypt.hash(password, 10);
+        await UserInstance.create({ email, password: hash, username, primary_time_zone });
+        res.json(factoryResponse(200, "Registration successful"));
+        console.log("User registered successfully");
+    }
+
+    catch (error) {
+        console.error("Error registering user:", error);
+        res.status(500).json(factoryResponse(500, "Internal Server Error at resgister"));
+    }
 };
 
 // Login route.
 // This route checks the user's credentials and logs them in.
 export const login = async (req, res, next) => {
-    const { email, password } = req.body;
-    const user = await UserInstance.findOne({ where: { email } });
-    if (!user) {
-        return res.status(401).json(factoryResponse(401, "User had not existed, please register first"));
-    }
-    if (!(await bcrypt.compare(password, user.password))) {
-        return res.status(401).json(factoryResponse(401, "Invalid credentials"));
-    }
-    //everything is ok now proceeds to include tokens for response
+    try {
+        const { email, password } = req.body;
+        const user = await UserInstance.findOne({ where: { email } });
+        if (!user) {
+            return res.status(401).json(factoryResponse(401, "User had not existed, please register first"));
+        }
+        if (!(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json(factoryResponse(401, "Invalid credentials"));
+        }
+        //everything is ok now proceeds to include tokens for response
 
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.setHeader('Authorization', `Bearer ${token}`);
-    res.json({
-        status: 200,
-        message: "Login successful",
-    });
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.setHeader('Authorization', `Bearer ${token}`);
+        res.json({
+            status: 200,
+            message: "Login successful",
+        });
+    }
+
+    catch (error) {
+        console.error("Error logging in user:", error);
+        res.status(500).json(factoryResponse(500, "Internal Server Error at login"));
+    }
 };
 
 
