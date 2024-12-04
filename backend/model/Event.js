925814393d1a69c3763a628348d1d86de8d37b74
joshua041904 <joshuaswaida@gmail.com>
2024-12-04 02:51:17 -0500

    Event Creation Route #59 : Added invitees field to EventsModel to store list of invitees as JSON

--- backend/model/Event.js
+++ backend/model/Event.js
@@ -1,38 +1,43 @@
 // import { Sequelize, DataTypes } from "sequelize";
-import { Sequelize, DataTypes, sql } from '@sequelize/core';
-import { SqliteDialect } from '@sequelize/sqlite3';
-
+import { Sequelize, DataTypes, sql } from "@sequelize/core";
+import { SqliteDialect } from "@sequelize/sqlite3";
 
 // Define the User model
 //Some Problems to look into: data type to store the timezone so that later you can easily convert it to the UTC.
 
 export function EventsModel(sequelize) {
-    const Events = sequelize.define("Events", {
-        event_id: {
-            type: DataTypes.UUID.V4,
-            defaultValue: sql.uuidV4,
-            primaryKey: true,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        description: {
-            type: DataTypes.STRING,
-        },
-        event_time: {
-            type: DataTypes.DATE,
-            allowNull: true
-        },
-        file_attachment: {
-            type: DataTypes.STRING, //TODO: may need to change here
-        },
-        // organizer_id:{}
-
-    }, {
-        timestamps: true, // Enable timestamps for createdAt and updatedAt
-    },);
-    return Events;
+  const Events = sequelize.define(
+    "Events",
+    {
+      event_id: {
+        type: DataTypes.UUID.V4,
+        defaultValue: sql.uuidV4,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+      invitees: {
+        type: DataTypes.JSON,
+        allowNull: true,
+      },
+      event_time: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      file_attachment: {
+        type: DataTypes.STRING, //TODO: may need to change here
+      },
+      // organizer_id:{}
+    },
+    {
+      timestamps: true, // Enable timestamps for createdAt and updatedAt
+    }
+  );
+  return Events;
 }
-
