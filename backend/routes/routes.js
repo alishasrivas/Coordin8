6c369a96a043b12d2d1ba7d27ce8df9b9dc972d9
Alisha Srivastava <68420427+alishasrivas@users.noreply.github.com>
2024-12-08 19:30:21 -0500

    Update routes.js to route updateEvent functon #88
    Close #88

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -9,7 +9,8 @@ import {
     createEvent,
     updateUserProfile,
     getUserProfile,
-    deleteEventInstance
+    deleteEventInstance,
+    updateEvent
 } from "../controller/controller.js";
 import { isAuthenticated, authenticatedJWT } from "../middleware/middleware.js";
 
@@ -29,6 +30,7 @@ router.get("/protected", authenticatedJWT, (req, res) => {
 // When an HTTP POST request is made to the /events endpoint with a valid JWT, the createEvent function will be called
 router.post("/events", authenticatedJWT, createEvent);
 router.delete("/events/:idofevent", authenticatedJWT, deleteEventInstance);
+router.update("/events/:eventid", authenticatedJWT, updateEvent);
 
 //endpoints for profile settings features
 router.patch('/userInfo/:id', authenticatedJWT, updateUserProfile);
